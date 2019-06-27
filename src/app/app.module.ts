e9b8bd0f52f4f5598a69a97ff67fc6f0d8059565
Дежурный <C:\Users\.-\AppData\Roaming\The Bat!>
2019-06-27 18:06:02 +0300

    lesson3,4 about form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {MyFirstComponentComponent} from "./components/myfirstcomponent/myfirstcomponent.component";
+import { TodoComponent } from './components/todo/todo.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyFirstComponentComponent
+    MyFirstComponentComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
