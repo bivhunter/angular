34bfaf1f83591f4db7388a42db9df4667ff9b201
Дежурный <C:\Users\.-\AppData\Roaming\The Bat!>
2019-06-27 20:03:55 +0300

    go home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {MyFirstComponentComponent} from "./components/myfirstcomponent/myfirstcomponent.component";
+import { MyFirstComponentComponent } from "./components/myfirstcomponent/myfirstcomponent.component";
 import { TodoComponent } from './components/todo/todo.component';
 import { FormsModule } from "@angular/forms";
+import { HomeComponent } from './components/home/home.component';
+import { FormComponent } from './components/form/form.component';
+import { ListComponent } from './components/list/list.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
+import { JsonplaceholderService} from "./services/jsonplaceholder.service";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
     MyFirstComponentComponent,
-    TodoComponent
+    TodoComponent,
+    HomeComponent,
+    FormComponent,
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [JsonplaceholderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
